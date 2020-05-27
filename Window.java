313fbd3673a6babd48e4143984094ee9532791bf
mariaxboza <65983691+mariaxboza@users.noreply.github.com>
2020-05-27 11:52:27 -0400

    Update Window.java

--- Window.java
+++ Window.java
@@ -1,11 +1,21 @@
 package game;
 import javax.swing.*;
 
+	/*
+	 * This class is to use JOptionPane
+	 */
 public class Window {
 	
+	/*
+	 * Method to print out a message in a window
+	 */
 	public void msg(String msg) {
         JOptionPane.showMessageDialog(null, msg);
-    }
+    	}
+	
+	/*
+	 * Method to print out options for the player to choose
+	 */
 
     public int option(String[] options, String msg) {
         return JOptionPane.showOptionDialog(null, msg,// my message
@@ -17,10 +27,16 @@ public class Window {
                 options[0]); // default option
     }
 
+	/*
+	 * Method to print out a message
+	 */
     public void println(String msg) {
         System.out.println(msg);
     }
 
+	/*
+	 * Method to get an input from the user
+	 */
     public String input(String msg) {
         String s = JOptionPane.showInputDialog(msg);
         return s;
